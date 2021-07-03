eb41f1b2a1b0a15ca50cced9d0fa04e99200ad70
Francisco de Assis <a124fr@gmail.com>
2021-07-03 00:18:18 -0300

    Outra forma de usar passagem de parametros entre o controle e a view

--- src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -1,15 +1,22 @@
 package br.com.alura.mvc.mudi.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 	
+//	@GetMapping("/hello")
+//	public String hello(Model model) {
+//		model.addAttribute("nome", "Mundo");
+//		return "hello";
+//	}
+	
 	@GetMapping("/hello")
-	public String hello(Model model) {
-		model.addAttribute("nome", "Mundo");
-		return "hello";
+	public ModelAndView hello() {
+		ModelAndView mv = new ModelAndView("hello");
+		mv.addObject("nome","Mundo");
+		return mv;
 	}
 }
