3b5655570732bcefed0612224bc4253a05304966
Francisco de Assis <a124fr@gmail.com>
2021-07-02 23:54:54 -0300

    Trabalhado com repository no spring mvc e jpa

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,26 +1,24 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("/home")
-	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 8");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/51Fv-PIiDQL._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-4RAM/dp/B07Y9XWK4M/ref=asc_df_B07Y9XWK4M/?tag=googleshopp00-20&linkCode=df0&hvadid=379792730378&hvpos=&hvnetw=g&hvrand=3818959471528278365&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9074242&hvtargid=pla-824195334382&psc=1");
-		pedido.setDescricao("Produto");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+	public String home(Model model) {						
+		List<Pedido> pedidos = this.pedidoRepository.recuperarTodosOsPedidos();
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
